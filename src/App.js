ad11cd7b3fb7b98c35af29190471ecb88ad4eb46
Ben Clark <ben.clark@palace.cl>
2025-07-17 09:22:48 -0400

    feat: Add Amplify authenticator UI debug 8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-// src/App.js
+// src/App.jsx
 import React from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
-  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  const { user, signOut } = useAuthenticator();
 
   return (
     <div>
       <h1>PDF Summarizer - AWS Version</h1>
-      <h2>Welcome, {user.attributes.email}</h2>
-      <button onClick={signOut}>Sign Out</button>
+
+      {/* This conditionally renders content only when the user is signed in */}
+      {user ? (
+        <div>
+          <h2>Welcome, {user.attributes.email}</h2>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      ) : (
+        <p>Please sign in to continue.</p>
+      )}
+
       <hr />
       <p>Your application content will go here.</p>
     </div>
